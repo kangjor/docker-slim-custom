3402f0c6d26ef02ad5bab5966368bdaa3b6fcac4
Kenneth <kenneth@zeroweb.kr>
2016-08-22 19:08:54 +0900

    fixed character encoding problem

--- slim/index.php
+++ slim/index.php
@@ -1,4 +1,6 @@
 <?php
+// header('Content-Type: application/json; charset=utf-8');
+
 require '../vendor/autoload.php';
 
 // config setting
@@ -14,6 +16,8 @@ $container['db'] = function ($c) {
     $db = $c['settings']['db'];
     $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
         $db['user'], $db['pass']);
+    // fixed character encoding problem
+    $pdo -> exec("set names utf8");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $pdo;
