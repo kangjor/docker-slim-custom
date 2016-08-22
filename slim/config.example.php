b291bc6bd47596cb9fffa8142c29f70d0961048b
Kenneth <kenneth@zeroweb.kr>
2016-08-22 18:43:20 +0900

    added mysql connection settings

---
+++ slim/config.example.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * config settings
+ */
+
+$config['displayErrorDetails'] = false;
+$config['addContentLengthHeader'] = false;
+
+$config['db']['host']   = "localhost";
+$config['db']['user']   = "user";
+$config['db']['pass']   = "password";
+$config['db']['dbname'] = "dbname";
--- slim/index.php
+++ slim/index.php
@@ -1,14 +1,37 @@
 <?php
 require '../vendor/autoload.php';
 
-$app = new \Slim\App();
+// config setting
+require './config.php';
+
+$app = new \Slim\App(["settings" => $config]);
+
+// to get dependency injection container
+$container = $app->getContainer();
+
+// PDO database library
+$container['db'] = function ($c) {
+    $db = $c['settings']['db'];
+    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
+        $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+
 
 $app->get('/', function () {
    print "API Version 1.0";
 });
 
 $app->get('/list', function () {
-   print "some list here";
+   // print "some list here";
+
+   $sth = $this->db->prepare("SELECT * FROM site_member_list ORDER BY seq");
+    $sth->execute();
+    $results = $sth->fetchAll();
+    return $this->response->withJson($results);
+
 });
 
 $app->get('/name/{name}', function ($request, $response, $args) {
